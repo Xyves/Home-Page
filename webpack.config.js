2b92881ec9ebd56ee35ec408c82c2f7f4d4c35b2
Xyves <xodrixx@protonmail.com>
2024-02-18 19:01:47 +0100

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,8 @@ const config = {
         use: [stylesHandler, "css-loader", "sass-loader"],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+        type: "asset/resource",
       },
 
       // Add your rules for custom modules here
